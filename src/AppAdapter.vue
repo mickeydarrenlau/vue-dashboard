1c57679a1bd46b9cb98633e0ce1eaeb691de1632
mickeydarrenlau <darrenwjlau@gmail.com>
2022-11-01 15:16:01 +0800

    Create AppAdapter.vue

---
+++ src/AppAdapter.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref, provide } from 'vue'
+import { app } from "./App.vue"
+const list_data = ref([{ name1: 'Bluemap Creative', image1: "https://rv.darren.ee.eu.org/thumbs/bluemap.jpg", url1:"https://rv.darren.ee.eu.org/bluemapcreative/", name2: 'Homeassistant', image2: "https://rv.darren.ee.eu.org/thumbs/homeassistant.jpg", url2:"https://hass.darrenmc.ee.eu.org"}])
+provide('list_items', list_data)
+</script>
+
+<template>
+<App/>
+</template>
