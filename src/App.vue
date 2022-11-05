69300d5e6a8966fd201195f03239fb6337ddb2f5
mickeydarrenlau <darrenwjlau@gmail.com>
2022-11-06 04:33:41 +0800

    Change li to div

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const items = inject('list_items')
 
 <template>
 	<table class="links">
-	  <li v-for="(item, index) in items">
+	  <div v-for="(item, index) in items">
 	  <div>
       <div v-if="item.url1 && item.name1 && item.image1" id="box" style="display: table-cell;" ><a :href="item.url1"><div class="box"><img :src="item.image1" alt="" height="70" width="70"/><div class="txt">{{ item.name1 }}</div></div></a></div>
        <div v-if="item.url2 && item.name2 && item.image2" id="box" style="display: table-cell;" ><a :href="item.url2"><div class="box"><img :src="item.image2" alt="" height="70" width="70"/><div class="txt">{{ item.name2 }}</div></div></a></div>
@@ -16,6 +16,6 @@ const items = inject('list_items')
          <div v-if="item.url4 && item.name4 && item.image4" id="box" style="display: table-cell;" ><a :href="item.url4"><div class="box"><img :src="item.image4" alt="" height="70" width="70"/><div class="txt">{{ item.name4 }}</div></div></a></div>
           <div v-if="item.url5 && item.name5 && item.image5" id="box" style="display: table-cell;" ><a :href="item.url5"><div class="box"><img :src="item.image5" alt="" height="70" width="70"/><div class="txt">{{ item.name5 }}</div></div></a></div>
           </div>
-	  </li>
+	  </div>
 	</table>
 </template>
