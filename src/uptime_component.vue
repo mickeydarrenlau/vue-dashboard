630ea37b46eb6a4bee1d8f6c2a6bb4d70c0580f2
mickeydarrenlau <darrenwjlau@gmail.com>
2022-11-06 13:04:06 +0800

    Update uptime_component.vue

--- src/uptime_component.vue
+++ src/uptime_component.vue
@@ -1,3 +1,9 @@
 <link rel="icon" href="favicon.ico" type="image/x-icon">
 <link rel="stylesheet" type="text/css" href="style.css">
 <link rel="style" href="/src/style.css" />
+<script setup>
+const props = defineProps(['url'])
+</script>
+
+
+
