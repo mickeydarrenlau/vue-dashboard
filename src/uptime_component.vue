4785dff7fcea6f42364f886744a3237d3b2ef19c
mickeydarrenlau <darrenwjlau@gmail.com>
2022-11-07 15:41:19 +0800

    Update uptime_component.vue

--- src/uptime_component.vue
+++ src/uptime_component.vue
@@ -3,6 +3,17 @@
 <link rel="style" href="/src/style.css" />
 <script setup>
 const props = defineProps(['url'])
+async function makeRequest() {
+  try {
+    const response = await fetch('https://randomuser.me/api/');
+
+    return response.status;
+
+  } catch (err) {
+    return err;
+  }
+}
+
 </script>
 
 
