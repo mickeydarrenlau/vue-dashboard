b3e785b28bcb3866d8ada6fd8999549cac2de70e
mickeydarrenlau <darrenwjlau@gmail.com>
2022-11-09 06:12:03 +0800

    Update uptime_component.vue

--- src/uptime_component.vue
+++ src/uptime_component.vue
@@ -19,7 +19,7 @@ const status = ref(d)
 </script>
 <template>
 <a style="color: green;" v-if="status == 200">&#183;</a>
-<v-else a style="color: red;">&#183;</a>
+<a v-else style="color: red;">&#183;</a>
 
 </template>
 
