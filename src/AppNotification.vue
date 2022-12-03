10d42b1829f8a8e3a6e723c242f797908911af1d
mickeydarrenlau <darrenwjlau@gmail.com>
2022-12-03 19:11:06 +0800

    Update AppNotification.vue

--- src/AppNotification.vue
+++ src/AppNotification.vue
@@ -4,8 +4,6 @@
 <script setup>
 import { ref } from 'vue'
 const noti = ref([])
-// Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
 
     var pusher = new Pusher('d617bc28d908c6454cbb', {
       cluster: 'ap1'
@@ -20,7 +18,7 @@ const noti = ref([])
 <template>
 <div class="txt">Notifications</div>
         <div v-for="(c) in noti">
-          <div id="box" style="display: table-cell;" ><div class="box"><img :src="c.image" alt="" height="70" width="70"/><div class="txt"> {{ c.message }} </div></div></div>
+          <div id="box" style="display: table-cell;" ><div class="box"><img :src="c.image" alt="" height="70" width="70"/><div height="80" width="80" class="txt"> {{ c.message }} </div></div></div>
           <br>
         </div>
 </template>
