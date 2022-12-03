445f12a783041ef66ad9f3b1f26a6ea8ca90d80c
mickeydarrenlau <darrenwjlau@gmail.com>
2022-12-03 08:05:20 +0800

    Update AppNotification.vue

--- src/AppNotification.vue
+++ src/AppNotification.vue
@@ -1,3 +1,6 @@
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="style" href="/src/style.css" />
 <script setup>
 import { ref } from 'vue'
 const noti = ref([])
